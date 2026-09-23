41db7d5be6b6ae6b05f86d38c56050b302327359
DenisBode <Denis.Bode@gmx.de>
2026-09-23 20:09:51 +0200

    Add world class and movableObject; fix script reference in index.html

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
 let canvas;
 let ctx;
-let character = new Character();
-let enemies = [new Chicken(), new Chicken(), new Chicken()];
+
 
 function init() {
     canvas = document.getElementById('canvas');
